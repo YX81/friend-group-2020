122e015a2e37162b465e564bdecd9cc3029e98e2
Yanyue Xiao <ucapxia@ucl.ac.uk>
2020-10-27 15:02:16 +0800

    select people

--- group.py
+++ group.py
@@ -34,3 +34,37 @@ my_group ={
         }
     }
 }
+age0=0
+for key in group:
+    age=group[key]["age"]
+    if age > age0:
+        name=key
+    age0=age
+
+    
+rel=0
+name_number=0
+for name,people in group.items():
+    rel+=len(people["relations"])
+    name_number+=1
+    average_rel=rel/name_number
+
+    
+age0=0
+for name,people in group.items():
+    age=people["age"]
+    if age > age0 and len(people["relations"])>=1:
+        name=name
+    age0=age 
+
+    
+age0=0
+for name,people in group.items():
+    age=people["age"]
+    for r in people["relations"].values():
+        nu=0
+        if r=="friend":
+            nu=nu+1
+        if age > age0 and nu >= 1:
+            nam=name
+    age0=age 
